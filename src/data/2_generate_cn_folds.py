3600bc427a3bdc83c90fd54c7b2ee66380b4a64b
Tiago Pinho <tiagopinhods@gmail.com>
2021-02-10 13:35:08 -0300

    created script to generate cn folds

--- src/data/2_generate_cn_folds.py
+++ src/data/2_generate_cn_folds.py
@@ -18,7 +18,7 @@ def create_folder(path, folder_name):
         logger.info('Folder already exist.')
     return path   
 
-def neighbors_to_remove(area, indexes, matrix, data):
+def neighbors_to_remove(indexes, matrix, data):
     matrix.index = matrix.index.astype(str)
     area_matrix = matrix.loc[indexes]
     neighbors = area_matrix.sum(axis=0) > 0
@@ -27,35 +27,17 @@ def neighbors_to_remove(area, indexes, matrix, data):
     to_remove = [n for n in neighbors if n not in indexes]
     return to_remove
 
-def get_geo_attribute(type_folds):
-    logger = logging.getLogger(__name__)
-    if type_folds == 'R':
-        geo_group = 'GEO_Cod_Grande_Regiao'
-    elif type_folds == 'S':
-        geo_group = 'GEO_Nome_UF'
-    elif type_folds == 'ME':
-        geo_group = 'GEO_Cod_Meso'
-    elif type_folds == 'MI':
-        geo_group = 'GEO_Cod_Micro'
-    elif type_folds == 'D':
-        geo_group = 'GEO_Cod_Distrito'
-    elif type_folds == 'SD':
-        geo_group = 'GEO_Cod_Subdistruto'
-    else:
-        geo_group = None
-        logger.info('Incorrect type fold option try: [R, S, ME, MI, D, SD, CN]')
-        exit()
-    return geo_group
 
 def generate_bipartite_matrix(data, adj_m_queen, center_candidate, n_neighbors):
-    cities_haddad = data[data['ELECTION_who_won'] == 'HADDAD'].GEO_Cod_Municipio.astype('str').values
-    cities_bolsonaro = data[data['ELECTION_who_won'] == 'BOLSONARO'].GEO_Cod_Municipio.values
-    
-    
+   
     if center_candidate == 'HADDAD':
-        adj_m_bipartite = adj_m_queen.loc[cities_bolsonaro, cities_haddad]
-    else:
+        cities_bolsonaro = data[data['ELECTION_who_won'] == 'BOLSONARO'].GEO_Cod_Municipio.astype('str').values
+        cities_haddad = data[data['ELECTION_who_won'] == 'HADDAD'].GEO_Cod_Municipio.values
         adj_m_bipartite = adj_m_queen.loc[cities_haddad, cities_bolsonaro]
+    else:
+        cities_haddad = data[data['ELECTION_who_won'] == 'HADDAD'].GEO_Cod_Municipio.astype('str').values
+        cities_bolsonaro = data[data['ELECTION_who_won'] == 'BOLSONARO'].GEO_Cod_Municipio.values
+        adj_m_bipartite = adj_m_queen.loc[cities_bolsonaro, cities_haddad]
     
     change_cities = adj_m_bipartite.sum(axis=1) > n_neighbors    
     change_cities = [int(id) for id in change_cities.index if change_cities.loc[id] == True]
@@ -102,7 +84,7 @@ def make_folds_by_changing_neighborhood(input_filepath, queen_matrix_filepath, m
         data = filter_data(data, adj_m_bipartite,  output_filepath)
         
     fold_output_filepath = create_folder(output_filepath, 'folds')    
-    for center_city_idx, row in tqdm(adj_m_bipartite.iterrows()):
+    for center_city_idx, row in tqdm(adj_m_bipartite.iterrows(), total=len(adj_m_bipartite)):
         test_idx =  row >= 1
         test_idx = test_idx[test_idx].index.values.tolist()
         test_idx.append(str(center_city_idx))
@@ -112,19 +94,45 @@ def make_folds_by_changing_neighborhood(input_filepath, queen_matrix_filepath, m
         train_data = data.copy()
             
         train_data.drop(test_data.index, inplace=True)
-        buffer = neighbors_to_remove(center_city_idx, test_data.index, adj_m_queen, data)
+        buffer = neighbors_to_remove(test_data.index, adj_m_queen, data)
         train_data.drop(buffer, inplace=True)
             
         test_data.to_csv(join(fold_path, 'test.csv'))
         train_data.to_csv(join(fold_path, 'train.csv'))
+    return output_filepath
     
+ 
+def plot_cn_groups(input_filepath, meshblock_filepath, queen_matrix_filepath, output_filepath, center_candidate, n_neighbors):
+    data = pd.read_csv(input_filepath)
+    adj_m_queen = pd.read_csv(queen_matrix_filepath)
+    meshblocks = gpd.read_file(meshblock_filepath)
     
+    if data.columns[0] == 'GEO_Cod_Municipio':
+        index_name = 'CD_GEOCMU'
+        data[index_name] = data.GEO_Cod_Municipio.astype('str')
+    elif data.columns[0] == 'GEO_Cod_ap':
+        index_name = 'Cod_ap'
+        data[index_name] = data.GEO_Cod_ap.astype('str')
+    
+    adj_m_queen.set_index(index_name, inplace=True)
+    
+    meshblocks = meshblocks.merge(data[['ELECTION_who_won', index_name]], on=index_name, how='left')
+    meshblocks.set_index(index_name, inplace=True)
+    
+    adj_m_bipartite = generate_bipartite_matrix(data, adj_m_queen, center_candidate, n_neighbors)
 
-
+    filtered_cities = adj_m_bipartite.index.astype(str).values.tolist() + adj_m_bipartite.columns.values.tolist()
+    center_cities = adj_m_bipartite.index.astype(str).values.tolist()
+    meshblocks.loc[center_cities, 'ELECTION_who_won'] = 'Center City'
+    meshblocks = meshblocks.loc[filtered_cities]
+    _, ax = plt.subplots(1, 1)
+    meshblocks.plot(column='ELECTION_who_won', cmap='Paired', legend=True, ax=ax)
+    plt.savefig(join(output_filepath, 'CN_folds.pdf'))
+    
     
 def run(input_filepath, meshblock_filepath, output_filepath, type_folds, queen_matrix_filepath, n_neighbors, center_candidate, filter_train):
     # Log text to show on screen
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
     logging.basicConfig(level=logging.INFO, format=log_fmt)
-    make_folds_by_changing_neighborhood(input_filepath, queen_matrix_filepath, meshblock_filepath, output_filepath, type_folds, center_candidate, n_neighbors, filter_train)
-    #plot_geo_groups(input_filepath, meshblock_filepath, output_filepath, type_folds)
+    output_filepath = make_folds_by_changing_neighborhood(input_filepath, queen_matrix_filepath, meshblock_filepath, output_filepath, type_folds, center_candidate, n_neighbors, filter_train)
+    plot_cn_groups(input_filepath, meshblock_filepath, queen_matrix_filepath, output_filepath, center_candidate, n_neighbors)
